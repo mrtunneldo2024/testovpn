2b2ddd923b67480053c51746bdddda4c99600fc4
mrtunneldo2024 <167242799+mrtunneldo2024@users.noreply.github.com>
2024-04-21 22:23:31 +0300

    Update cf.sh

--- data/cf.sh
+++ data/cf.sh
@@ -84,10 +84,10 @@ touch /etc/domain/d-domain
 touch /etc/domain/f-domain
 clear
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
-DOMAIN=ilyass.my.id
-WS_DOMAIN=ws-${sub}.ovpn.ilyass.my.id
-FLARE_DOMAIN=flare-${sub}.ovpn.ilyass.my.id
-NS_DOMAIN=ns-${sub}.ilyass.my.id
+DOMAIN=mrtunnelvpn.xyz
+WS_DOMAIN=ws-${sub}.ovpn.mrtunnelvpn.xyz
+FLARE_DOMAIN=flare-${sub}.ovpn.mrtunnelvpn.xyz
+NS_DOMAIN=ns-${sub}.mrtunnelvpn.xyz
 CF_ID=mahmoud.rabiee.elsayed@gmail.com
 CF_KEY=2111d694773708c0e603c346ef9c97fd4d83c
 set -euo pipefail
